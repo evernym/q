01df85050adbec3efbda71984a5819088cc13cbf
Daniel Hardman <daniel.hardman@gmail.com>
2018-12-08 21:38:04 -0700

    Add sample emails
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- mailagent/test/test_mail_transport.py
+++ mailagent/test/test_mail_transport.py
@@ -30,7 +30,7 @@ class TransportTestCase(unittest.TestCase):
 
     def test_receive(self):
         # Right now, this only passes if the inbox isn't empty.
-        pass #self.assertTrue(self.t.receive())
+        pass #self.assertTrue(t.receive())
 
 if __name__ == '__main__':
     unittest.main()
