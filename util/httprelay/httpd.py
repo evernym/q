1ccfdd0af3d5b132c09a504d2a0d4a75123ad313
Daniel Hardman <daniel.hardman@gmail.com>
2019-01-30 03:53:42 -0700

    improved httprelay
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- util/httprelay/httpd.py
+++ util/httprelay/httpd.py
@@ -7,6 +7,8 @@ import re
 import traceback
 import cgi
 import urllib
+import threading
+import time
 
 import os
 import sys
@@ -23,11 +25,12 @@ for folder in IMPORT_FOLDERS:
         break
 import file_transport
 
-PORT = 8000
+PORT = 8001
 
-PENDING_PAT = re.compile('(?i)/pending/([a-f0-9]{8}-(?:[a-f0-9]{4}-){3}[a-f0-9]{12})$')
-IN_PAT = re.compile('(?i)/in/?$')
-OUT_PAT = re.compile('(?i)/out/([a-f0-9]{8}-(?:[a-f0-9]{4}-){3}[a-f0-9]{12})$')
+PENDING_PAT = re.compile('(?i)/pending/([a-f0-9]{8}-(?:[a-f0-9]{4}-){3}[a-f0-9]{12})(?:[?]wait=(1|0|true|false))?$')
+IN_PAT = re.compile('(?i)/req/?$')
+OUT_PAT = re.compile('(?i)/resp/([a-f0-9]{8}-(?:[a-f0-9]{4}-){3}[a-f0-9]{12})$')
+FALSE_PAT = re.compile('(?i)(0|false)')
 
 def _match_special(path):
     for pat in [PENDING_PAT, IN_PAT, OUT_PAT]:
@@ -36,7 +39,15 @@ def _match_special(path):
             return m, pat
     return None, None
 
+# Declare a global variable so we don't have to create a new instance of the
+# transport for every Handler instance. Also, declare a mutex to protect it,
+# since it is not thread-safe, but we will be calling its methods from multiple
+# threads (one for each Handler instance). This allows each Handler to block
+# as long as it needs to, without blocking other Handlers that are created to
+# service new incoming threads.
 trans = file_transport.FileTransport(os.path.expanduser('~/.httprelay/queue'))
+# Any time we invoke methods of trans, mutex by acquiring this lock.
+trans_lock = threading.Lock()
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
@@ -45,10 +56,29 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             m, pat = _match_special(self.path)
             if m:
                 id = m.group(1)
-                ready = trans.peek(id)
+                # Try to get the requested item. If we're asking for
+                # /pending/something, and we haven't been told to skip
+                # the wait, then wait for up to 30 seconds to get a
+                # a final status. This facilitates long polling in a UI.
+                wait = False
+                if pat == PENDING_PAT:
+                    wait = m.group(2)
+                    if wait:
+                        wait = not bool(FALSE_PAT.match(wait))
+                    else:
+                        wait = True
+                elapsed = 0
+                while elapsed <= 30:
+                    with trans_lock:
+                        ready = trans.peek(id)
+                    if ready or not wait:
+                        break
+                    time.sleep(1.0)
+                    elapsed += 1
                 if pat == OUT_PAT:
                     if ready:
-                        mwc = trans.receive(id)
+                        with trans_lock:
+                            mwc = trans.receive(id)
                         self.send_response(200)
                         self.send_header('Content-Type', 'application/ssi-agent-wire')
                         self.send_header('Content-Length', len(mwc.msg))
@@ -58,9 +88,9 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                         self.send_error(404, 'Response not yet available.')
                 elif pat == PENDING_PAT:
                     if ready:
-                        self.send_status(303, {'Location':'/out/' + id})
+                        self.send_status(303, {'Location':'/resp/' + id})
                     else:
-                        self.send_status(200)
+                        self.send_status(200, {'Location':'/pending/' + id})
                 else:
                     self.send_error(405, explain='You must use POST with %s' % self.path)
             else:
@@ -91,7 +121,8 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                             if matches:
                                 txt = matches[0]
                     if txt:
-                        id = trans.send(txt)
+                        with trans_lock:
+                            id = trans.send(txt)
                         link = '/pending/' + id
                         self.send_status(202, {'Location':link})
                     else:
@@ -116,12 +147,15 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(txt.encode("utf-8"))
 
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
 if __name__ == '__main__':
     # Change the format of error messages so they use our preferred style.
     with open(os.path.join(MY_FOLDER, 'error.html'), 'rt') as f:
         http.server.BaseHTTPRequestHandler.error_message_format = f.read()
     # Run a server until Ctrl+C
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    with ThreadedTCPServer(("", PORT), Handler) as httpd:
         try:
             try:
                 print("Serving at port %s..." % PORT)
